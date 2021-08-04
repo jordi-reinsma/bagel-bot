2f00af17403f9d427b2d035205007c7b7721af02
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2021-08-04 17:26:40 -0300

    Fix `SlackClient::send`

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,26 @@
 mod client;
 mod method;
-mod partition;
 
-use std::env;
-use std::collections::HashMap;
+use std::{collections::HashMap, env};
 
 use client::SlackClient;
+use method::Method;
 
 const CHANNEL_NAMES: &str = include_str!("../channel-names");
 
-fn main() {
-    let api_key = env::var("API_KEY").expect("API_KEY not found");
-    let client = SlackClient::from_key(&api_key);
+#[tokio::main]
+async fn main() {
+    // Get it at `https://api.slack.com/apps/<your-bot-id>/oauth?`
+    let oauth_token = env::var("SLACK_OAUTH_TOKEN").expect("SLACK_OAUTH_TOKEN not found");
+    let client = SlackClient::from_key(&oauth_token);
 
     let mut parameters = HashMap::new();
-    parameters.insert(
-        "channel",
-        "C025U17MQ0J",
-    );
-    parameters.insert(
-        "api-key",
-        api_key,
-    );
+    parameters.insert("channel", "C02A4KVB43F");
 
-    let result = client.send(method::Method::ListMembersOfChannel, parameters);
-    dbg!(result); // doest not work because result is a Future
+    let response = client
+        .send(Method::ListMembersOfChannel, parameters)
+        .await
+        .expect("request failed!");
+
+    dbg!(response);
 }
