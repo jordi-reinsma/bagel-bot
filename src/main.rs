55abd02a6a727359fd00c5282a5ffa1ea16b1c5a
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2021-08-05 14:38:30 -0300

    Refactor main function into `set_up_meetings`

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,36 @@
 mod client;
 mod error;
 mod method;
+mod partition;
 
-use std::{collections::HashMap, env};
+use std::env;
 
+use error::Result;
 use client::SlackClient;
-use method::Method;
 
-const CHANNEL_NAMES: &str = include_str!("../channel-names");
+const CHANNEL_IDS: &str = include_str!("../channel-ids");
 
 #[tokio::main]
 async fn main() {
-    // Get it at `https://api.slack.com/apps/<your-bot-id>/oauth?`
+    let channels: Vec<_> = CHANNEL_IDS.split('\n').collect();
+
+    for channel in channels {
+        let _ = dbg!(set_up_meetings(channel).await);
+    }
+}
+
+async fn set_up_meetings(channel_id: &str) -> Result<()> {
+    // Get your token at `https://api.slack.com/apps/<your-bot-id>/oauth?`
     let oauth_token = env::var("SLACK_OAUTH_TOKEN").expect("SLACK_OAUTH_TOKEN not found");
     let client = SlackClient::from_key(&oauth_token);
 
-    // let mut parameters = HashMap::new();
-    // parameters.insert("channel", "");
-
-    let users = client.members_of_channel("C02A4KVB43F").await.unwrap();
+    let users = client.members_of_channel(channel_id).await?;
     dbg!(&users);
 
-    let channel_id = client.start_direct_message(users).await.unwrap();
+    let channel_id = client.start_direct_message(users).await?;
     dbg!(&channel_id);
 
-    let success = client.post_message(&channel_id, "Olá, abigos!").await.unwrap();
-    dbg!(success);
+    let success = client.post_message(&channel_id, "Olá, abigos!").await?;
+    
+    Ok(())
 }
